ca0b46077b031c972ad93f72d5e48ca49aab5a58
ison.dell <ison@dell.com>
2016-11-28 23:57:08 +0800

    spring-session-redis Init

---
+++ spring-session-redis/src/main/java/cn/ptp/security/SecurityConfig.java
@@ -0,0 +1,37 @@
+package cn.ptp.security;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)//允许进入页面方法前检验
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class SecurityConfig extends WebSecurityConfigurerAdapter
+{
+    private final AuthProvider provider;//自定义验证
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        //http.authorizeRequests().antMatchers("/", "/home").permitAll()
+        http.authorizeRequests().antMatchers("/user/**").authenticated()
+        //        .anyRequest().authenticated()
+                .anyRequest().permitAll()
+                .and().formLogin().loginPage("/login").permitAll()
+                .and().logout().permitAll();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception
+    {
+        //auth.inMemoryAuthentication().withUser("admin").password("admin").roles("USER");
+        auth.authenticationProvider(provider);
+    }
+}
