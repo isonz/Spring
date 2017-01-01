68f08bfc1df6fc98b3d6d92b71bf300bd2598b6b
ison.apple <ison@apple.com>
2017-01-02 01:34:17 +0800

    spring cloud

--- spring-cloud/consumer_ribbon/src/main/java/cn/ptp/controller/ClientController.java
+++ spring-cloud/consumer_ribbon/src/main/java/cn/ptp/controller/ClientController.java
@@ -1,16 +1,16 @@
 package cn.ptp.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.cloud.client.ServiceInstance;
 
-import org.apache.log4j.Logger;
 import org.springframework.web.client.RestTemplate;
 
 
@@ -20,12 +20,24 @@ import org.springframework.web.client.RestTemplate;
 public class ClientController
 {
     private final Logger logger = Logger.getLogger(getClass());
-    private final RestTemplate restTemplate;;
+
+    private final RestTemplate restTemplate;
+    private final DiscoveryClient client;
 
     @RequestMapping(value = "/add" ,method = RequestMethod.GET)
     public String add() {
         System.out.println("test");
-        return restTemplate.getForEntity("http://client_test/add?a=10&b=20", String.class).getBody();
+        //ResponseEntity<String> response = restTemplate.getForEntity("http://CONSUMER-RIBBON/client/addd?a=10&b=20", String.class);
+        ResponseEntity<String> response = restTemplate.getForEntity("http://CLIENT-TEST/client/add?a=10&b=20", String.class);
+        return response.getBody();
+    }
+
+    @RequestMapping(value = "/addd" ,method = RequestMethod.GET)
+    public Integer addd(@RequestParam Integer a, @RequestParam Integer b) {
+        ServiceInstance instance = client.getLocalServiceInstance();
+        Integer r = a + b;
+        logger.info("/add, host:" + instance.getHost() + ", service_id:" + instance.getServiceId() + ", result:" + r);
+        return r;
     }
 
 }
